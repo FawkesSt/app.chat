f18b8592aa3b55382c5b9a24162dfc976017a01b
Matheus Santos <matheus.corp.santos@gmail.com>
2021-04-04 17:09:10 -0300

    starting project

---
+++ js/copyperfect.js
@@ -0,0 +1,124 @@
+
+function copyData() {
+    var CopyContent = `<table style="height: 1656px; margin-left: auto; margin-right: auto;" width="439">
+    <tbody>
+    <tr>
+    <td style="width: 429px;">
+    <div>
+    <div class="m_-1217793721356170494block-grid">
+    <div>
+    <div class="m_-1217793721356170494col">
+    <div class="m_-1217793721356170494col_cont">
+    <div>
+    <div align="center"><img class="CToWUd" title="Texto alternativo" src="https://ci3.googleusercontent.com/proxy/1n5KY7BKkAAHXVcDMbpth9SZZThLLtu8oZPUGGeYa8WSUq-R1NrAfHFCb86g6O2iPy7TmjNWNI7lIXhn2iiurZ_AAegBfMAsxgTygl679EzO2kum-_2N4jZC8ZnEllF-6H81iq_GygB2Kyo7-sU-u-gxkoP0ZF9a7t9jmEIg-HRe0BwnpeAZOiCl2ZEAOxJR9VJht9BN5Oau6hDoiP99zP4Wt2-eNrRTSNGIcEk=s0-d-e1-ft#https://d15k2d11r6t6rl.cloudfront.net/public/users/Integrators/9883114d-06e2-4ba9-b8e3-4323a341fd28/bee-leadlovers-uid-18341/img-18341-20160812144416.png" alt="Texto alternativo" width="192" align="center" border="0" />
+    <div>&nbsp;</div>
+    </div>
+    <div>
+    <div>
+    <p>&Agrave;s vezes sua estrat&eacute;gia n&atilde;o est&aacute; convertendo por uma raz&atilde;o que n&atilde;o &eacute; relacionada ao&nbsp;<strong>marketing</strong>&nbsp;nem &agrave;&nbsp;<strong>copy</strong>... mas sim ao&nbsp;<u>digital</u><strong>.</strong></p>
+    <p><br />Isso por que seu&nbsp;<strong>sucesso no mundo online</strong>&nbsp;n&atilde;o depende s&oacute; de voc&ecirc;.</p>
+    <p><br /><strong>Depende tamb&eacute;m de sua entrega.</strong></p>
+    <p><br />Veja. Voc&ecirc; elabora sua estrat&eacute;gia de marketing. Cria as copys. Deixa tudo pronto. A&iacute; chega a&nbsp;hora de entregar tudo isso ao seu p&uacute;blico.</p>
+    <p><br />Essa entrega &eacute; feita atrav&eacute;s de sua estrutura online formada por suas p&aacute;ginas e e-mails.</p>
+    <p><br />At&eacute; aqui tudo bem.</p>
+    <p><br />O problema &eacute; que se suas p&aacute;ginas demorarem pra carregar, estiverem quebradas ou fora do&nbsp;ar... pessoas deixar&atilde;o de receber seu marketing... e&nbsp;<strong>voc&ecirc; perde vendas</strong>.</p>
+    <p><br />E mais importante...</p>
+    <p><br />Se seus e-mails n&atilde;o forem entregues... a&iacute; voc&ecirc; realmente perde vendas. Muitas vendas.</p>
+    <p><br />Veja o caso de um de nossos clientes, o&nbsp;<strong>Gustavo Mota</strong>, CEO da We Do Logos.</p>
+    <p><br />Antes de vir pro leadlovers, ele usava uma plataforma muito conhecida no mercado (e uma&nbsp;das mais caras).</p>
+    <p>&nbsp;</p>
+    <p>A abertura dos e-mails dele era por volta de&nbsp;<strong>1% a 3%</strong>.</p>
+    <p><br />Depois de mudar para a leadlovers, as aberturas dos e-mails de Gustavo subiram para cerca de&nbsp;<strong>11% a 14%</strong>...</p>
+    <p><br /><strong>...e seu faturamento saiu de quase 0 para 80 mil reais por m&ecirc;s.</strong></p>
+    <p><br />Se quiser,&nbsp;<strong>assista a hist&oacute;ria do Gustavo aqui:</strong></p>
+    </div>
+    </div>
+    <div class="m_-1217793721356170494video-block" style="max-width: 530px; min-width: 300px; padding: 10px;" align="center">
+    <div>
+    <table style="table-layout: fixed; vertical-align: top; border-spacing: 0; border-collapse: collapse; background-image: url('https://ci4.googleusercontent.com/proxy/ICRHK6zeV0GHgZYCp0zJHArIXU6IRVyr1WB93OSxFFzNk13U2KwEVEF1khJheCgOSDOHLUE0BWvK96f96iKYn9hGPa9rwtcGGJjE=s0-d-e1-ft#https://img.youtube.com/vi/gppQXTsfsgQ/maxresdefault.jpg'); background-size: cover; min-height: 169px; min-width: 300px;" border="0" width="100%" cellspacing="0" cellpadding="0">
+    <tbody>
+    <tr style="vertical-align: top;" valign="top">
+    <td style="word-break: break-word; vertical-align: top;" valign="top" width="25%"><img class="CToWUd" style="text-decoration: none; border: 0; height: auto; opacity: 0; display: block;" src="https://ci5.googleusercontent.com/proxy/lCJ_npQsccWyuWfz3sK-31EHbfbBhQ0rDDB3W47azCB-qK_ynb80d1HKHnu7hx762wyUKEjfCocWANOcldTOX7vpOw=s0-d-e1-ft#https://beefree.io/img-host/video_ratio_16-9.gif" alt="" width="100%" border="0" /></td>
+    <td style="word-break: break-word; vertical-align: middle;" align="center" valign="middle" width="50%">
+    <div style="display: inline-block; vertical-align: middle; background-color: #ffffff; border: 3px solid #ffffff; height: 59px; width: 59px; border-radius: 100%;">
+    <div style="padding: 14.75px 22.69230769230769px;">
+    <div style="border-style: solid; border-width: 15px 0 15px 20px; display: block; font-size: 0; height: 0; width: 0; border-color: transparent transparent transparent #000000;">&nbsp;</div>
+    </div>
+    </div>
+    </td>
+    <td style="word-break: break-word; vertical-align: top;" valign="top" width="25%">&nbsp;</td>
+    </tr>
+    </tbody>
+    </table>
+    </div>
+    </div>
+    <div>
+    <div>
+    <p>O detalhe &eacute; que&nbsp;<strong>ele n&atilde;o mudou a estrat&eacute;gia, nem a copy.&nbsp; Mudou apenas de ferramenta (e&nbsp;</strong><strong>pagando menos por isso).</strong></p>
+    <p><br />N&atilde;o sei... Mas me pareceu um bom neg&oacute;cio para Gustavo.</p>
+    <p>&nbsp;</p>
+    <p>Obviamente n&atilde;o posso prometer que ao usar o leadlovers voc&ecirc; ter&aacute; resultados como os do&nbsp;Gustavo.</p>
+    <p><br />Mas eu posso lhe prometer que&nbsp;<strong>n&atilde;o foi sorte.</strong></p>
+    <p><br />Nosso cuidado e dedica&ccedil;&atilde;o com nossos servidores proporcionam aos nossos clientes uma das&nbsp;<strong>maiores taxas de entrega do mercado</strong>.</p>
+    <p>&nbsp;</p>
+    <p>E quanto mais os e-mails deles s&atilde;o entregues, mais&nbsp;vendas eles fazem.</p>
+    <p>&nbsp;</p>
+    <p><strong>Descubra tudo sobre o leadlovers clicando no bot&atilde;o abaixo:</strong></p>
+    </div>
+    </div>
+    <div align="center"><a href="https://lp.amoleads.com/infoprodutorm" target="_blank" rel="noopener noreferrer" data-saferedirecturl="https://www.google.com/url?q=https://lp.amoleads.com/infoprodutorm&amp;source=gmail&amp;ust=1612464195940000&amp;usg=AFQjCNEQIkQ7gOsOmWmjmYf1MAi-58ZqDQ"><strong>Descubra TUDO Sobre o Leadlovers ➤</strong></a></div>
+    <div>
+    <div>
+    <p>At&eacute; mais,</p>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    <div>
+    <div class="m_-1217793721356170494block-grid">
+    <div>
+    <div class="m_-1217793721356170494col m_-1217793721356170494num3">
+    <div class="m_-1217793721356170494col_cont">
+    <div>
+    <div align="center"><img class="CToWUd" title="Texto alternativo" src="https://ci4.googleusercontent.com/proxy/vrCQC8cPQRe3sPv2jjlZOGm1naH4V5d4yb2dyLQ-WMa7c44_oisx3-KO24xYYTF7qx17sZcrUwjtNwhnuijNBf3yACHL57yQxt-zlCRO9bhqAwNpeF-y8TzZlhUD6Jqr6Zg3IhM0U_O4s7qvcl9EtXwdBxBpF5fyHjw6yn_EwqTXSW-YYYz2hUFhvaKzLIyCTF_k2GIJUGWSHaEFw8bbSHwhXluK0lEZd8SGyoaqrbHfEGgMqskl=s0-d-e1-ft#https://d15k2d11r6t6rl.cloudfront.net/public/users/Integrators/9883114d-06e2-4ba9-b8e3-4323a341fd28/bee-leadlovers-uid-18341/circle-cropped%20%288%29%20%281%29.png" alt="Texto alternativo" width="89" align="center" border="0" /></div>
+    </div>
+    </div>
+    </div>
+    <div class="m_-1217793721356170494col m_-1217793721356170494num9">
+    <div class="m_-1217793721356170494col_cont">
+    <div>
+    <div>
+    <div>
+    <p><strong>Luiz Machado</strong></p>
+    <p>Equipe leadlovers</p>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </td>
+    </tr>
+    </tbody>
+    </table>`;
+var copi = $('<textarea>').val(CopyContent).appendTo('body').select()
+document.execCommand('copy');
+}
+
+
+function clearcheck() {
+    document.getElementById("dns").checked = false;
+    document.getElementById("relatorio").checked = false;
+    document.getElementById("blacklist").checked = false;
+    document.getElementById("funil").checked = false;
+    document.getElementById("copy-generate").checked = false;
+    document.getElementById("copy-result").checked = false;
+    document.getElementById("open-ticket").checked = false;
+}
+
---
+++ js/index.js
@@ -0,0 +1,65 @@
+let newField = (header, value = "") => `
+    <div class="field">
+        <button class="btn_delete" onclick="deleteField(this)">
+        X
+        </button>
+        <p contenteditable="true">${header}</p>
+        <input type="text" class=".inputcss" value="${value}"/>
+        <button id="btn0" class="btn_copy" onclick="copyField(this)">
+            Copiar
+        </button>
+    </div>`;
+
+let macro = document.querySelector("#search-macro");
+let addButton = document.querySelector(".add_button");
+let saveButton = document.querySelector(".save_button");
+let wrapper = document.querySelector(".field_wrapper");
+
+function deleteField(elem) {
+    const parent = elem.parentNode;
+    var answer = window.confirm("Tem certeza que deseja mardar sua nota para marte?");
+        if (answer) {
+            parent.parentNode.removeChild(parent);
+    }
+    else {
+        
+    }
+    
+}
+
+function copyField(elem) {
+    const parent = elem.parentNode;
+
+    navigator.clipboard.writeText(
+        parent.querySelector("input").value
+    );
+}
+
+addButton.addEventListener("click", _ => {
+    wrapper.innerHTML += newField(macro.value);
+});
+
+saveButton.addEventListener("click", _ => {
+    let fields = document.querySelectorAll(".field");
+
+    let save = [];
+
+    fields.forEach(item => {
+        save.push(
+            {
+                label: item.querySelector("p").innerHTML,
+                content: item.querySelector("input").value
+            }
+        );
+    });
+
+    localStorage.setItem("chat-assistant", JSON.stringify(save));
+});
+
+function getSave() {
+    let save = JSON.parse(localStorage.getItem("chat-assistant"));
+
+    save.forEach(item => {
+        wrapper.innerHTML += newField(item.label, item.content);
+    });
+}
