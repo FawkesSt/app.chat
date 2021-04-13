10615553e3947c37fbad1fe059251a91d2680d73
FawkesSt <matheus.corp.santos@gmail.com>
2021-04-12 22:55:06 -0300

    added new cards for design fix

---
+++ js/design.js
@@ -0,0 +1,85 @@
+const element1 = document.querySelector("#element1");
+const element2 = document.querySelector("#element2");
+const element3 = document.querySelector("#element3");
+const element4 = document.querySelector("#element4");
+const element5 = document.querySelector("#element5");
+const element6 = document.querySelector("#element6");
+const element7 = document.querySelector("#element7");
+const element8 = document.querySelector("#element8");
+const element9 = document.querySelector("#element9");
+const element10 = document.querySelector("#element10");
+const element11 = document.querySelector("#element11");
+const element12 = document.querySelector("#element12");
+
+
+
+function copyElem1 (){
+    element1.select();
+    document.execCommand('copy');
+}
+
+function copyElem2 (){
+    element2.select();
+    document.execCommand('copy');
+}
+
+
+function copyElem3 (){
+    element3.select();
+    document.execCommand('copy');
+}
+
+
+function copyElem4 (){
+    element4.select();
+    document.execCommand('copy');
+}
+
+
+function copyElem5 (){
+    element5.select();
+    document.execCommand('copy');
+}
+
+function copyElem6 (){
+    element6.select();
+    document.execCommand('copy');
+}
+
+
+function copyElem7 (){
+    element7.select();
+    document.execCommand('copy');
+}
+
+
+function copyElem8 (){
+    element8.select();
+    document.execCommand('copy');
+}
+
+
+function copyElem9 (){
+    element9.select();
+    document.execCommand('copy');
+}
+
+
+function copyElem10 (){
+    element10.select();
+    document.execCommand('copy');
+}
+
+
+function copyElem11 (){
+    element11.select();
+    document.execCommand('copy');
+}
+
+
+function copyElem12 (){
+    element12.select();
+    document.execCommand('copy');
+}
+
+
